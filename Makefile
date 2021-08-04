cd72d14932d9e8dee26c63e1b065cdcb2c8ae666
Keqing Li <me@kaze.ai>
2021-08-04 11:15:53 +0800

    skeleton

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	$(MAKE) -C sources
+
+clean:
+	$(MAKE) clean -C sources
---
+++ sources/main.cpp
@@ -0,0 +1,8 @@
+#include <final/final.h>
+
+int main(int argc, char * argv[])
+{
+    finalcut::FApplication app(argc, argv);
+    /// TODO: To be filled...
+    return app.exec();
+}
